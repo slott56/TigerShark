33025ce600a80db4a7f466a6087ff62d2b4bb3b0
Steven Buss <sbuss@counsyl.com>
2012-04-05 11:35:21 -0700

    Fixed imports in extras

--- tigershark/extras/standardSegment.py
+++ tigershark/extras/standardSegment.py
@@ -19,7 +19,7 @@ X12 Segments.
 ..  autofunction:: buildSegment
 """
 from __future__ import print_function
-from X12.parse import Segment, Element, Properties
+from tigershark.X12.parse import Segment, Element, Properties
 
 ISAFields= """  ISA01 Authorization Information Qualifier
   ISA02 Authorization Information
