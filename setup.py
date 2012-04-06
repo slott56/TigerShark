67f052662bea751cb0b8116b6114b43bb183985a
Steven Buss <sbuss@counsyl.com>
2012-04-05 18:30:50 -0700

    My colleagues have insisted that code generation in setup.py is a Bad Idea.

--- setup.py
+++ setup.py
@@ -1,53 +1,6 @@
-import os
 from distutils.core import setup
-from distutils.command.build_py import build_py
-import fnmatch
-import logging
-import zipfile
 
 from tigershark import __version__
-
-
-class GenerateParsers(build_py):
-    """ After building tigershark, use it to build the parsers.
-
-    This happens before everything in the build directory is copied to
-    site-packages, so this effectively installs global default parsers.
-    """
-    def run(self):
-        build_py.run(self)
-        from tigershark.tools import convertPyX12
-
-        def convert(filename):
-            data_ele_file = zipf.open("pyx12-1.5.0/map/dataele.xml")
-            codes_file = zipf.open("pyx12-1.5.0/map/codes.xml")
-            fname = "M%s.py" % filename.rsplit('/', 1)[1].rsplit('.', 1)[0].\
-                        replace(".", "_")
-            dest_fname = os.path.join(self.build_lib, "tigershark", "parsers",
-                    fname)
-            parser_name = "parsed_%s" % \
-                    filename.rsplit('/', 1)[1].split('.', 1)[0]
-            convertPyX12.writeFile(dest_fname, parser_name,
-                convertPyX12.convertFile(zipf.open(filename), data_ele_file,
-                    codes_file))
-            # ZipFile file-like objects don't support seek, so we have to
-            # close and re-open them every time :\
-            data_ele_file.close()
-            codes_file.close()
-
-        zipf = zipfile.ZipFile("Downloads/pyx12-1.5.0.zip")
-
-        filenames = [f for f in zipf.namelist() if "map" in f and "xml" in f]
-        for filename in filenames:
-            if fnmatch.fnmatch(filename.rsplit('/', 1)[1],
-                    "[0-9][0-9][0-9]*.4010.X*.xml"):
-                print(filename)
-                try:
-                    convert(filename)
-                except Exception as e:
-                    raise e
-                    print(e)
-                    logging.error(e)
 
 
 setup(
@@ -82,7 +35,7 @@ setup(
     ],
     scripts=[
         'tigershark/tools/convertPyX12.py',
+        'tigershark/tools/generate_all_parsers.py',
     ],
     package_data={'tigershark': ['tests/*.txt', 'tests/*.xml', 'tests/*.csv']},
-    cmdclass={"build_py": GenerateParsers},
 )
