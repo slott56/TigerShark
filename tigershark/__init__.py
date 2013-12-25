5081641f1b189a43e9eab4813256598cc0a79f6f
Steven Buss <sbuss@counsyl.com>
2013-12-24 22:33:58 -0800

    Version bump to 0.2.7 for pypi

--- tigershark/__init__.py
+++ tigershark/__init__.py
@@ -1,7 +1,7 @@
 """
 TigerShark - An X12 EDI message parser.
 """
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 __authors__ = [
         "Steven Buss <steven.buss@gmail.com>",
         "Steven Lott <slott56@gmail.com>",
